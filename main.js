d6a6ea9d629fb7a29a18bc50399f5c5c04fe3ae4
davidstiven123 <davidstiven123@gmail.com>
2022-08-18 15:02:56 -0500

    a

--- main.js
+++ main.js
@@ -383,14 +383,17 @@ class PruebaComponent {
     }
 }
 PruebaComponent.ɵfac = function PruebaComponent_Factory(t) { return new (t || PruebaComponent)(); };
-PruebaComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: PruebaComponent, selectors: [["app-prueba"]], decls: 3, vars: 2, consts: [[3, "adClient", "pageLevelAds"]], template: function PruebaComponent_Template(rf, ctx) { if (rf & 1) {
+PruebaComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: PruebaComponent, selectors: [["app-prueba"]], decls: 4, vars: 7, consts: [[3, "adClient", "pageLevelAds"], [3, "adClient", "adSlot", "display", "width", "height"]], template: function PruebaComponent_Template(rf, ctx) { if (rf & 1) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "prueba works!");
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](2, "ng-adsense", 0);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](3, "ng-adsense", 1);
     } if (rf & 2) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](2);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵproperty"]("adClient", "ca-pub-2169890752691639")("pageLevelAds", true);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](1);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵproperty"]("adClient", "ca-pub-2169890752691639")("adSlot", 3939566548)("display", "inline-block")("width", 320)("height", 108);
     } }, directives: [ng2_adsense__WEBPACK_IMPORTED_MODULE_1__["AdsenseComponent"]], styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2Fkcy9wcnVlYmEvcHJ1ZWJhLmNvbXBvbmVudC5jc3MifQ== */"] });
 /*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](PruebaComponent, [{
         type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"],
